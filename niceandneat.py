96fa7e177303aaa68c91db4405f5a1294924c67e
Joshwa2200 <badcompany445@gmail.com>
2024-10-14 16:09:28 -0500

    NiceandNeat3

--- niceandneat.py
+++ niceandneat.py
@@ -26,7 +26,12 @@ def main():
     characters= list (character_counts.keys())
 
     counts_of_characters= list (character_counts.values())
+
+    sorted_characters = sorted(character_counts.items(), key=lambda item: item[1], reverse=True)
+
+    for char, count in sorted_characters:
+        organized_characters=print(f"The '{char}' character was found {count} times")   
     
-    print(f" ---Begin Report of Frankenstein--- This book contains {word_count} words, and the letter {characters} appears {counts_of_characters} times. ---End Report---")
+    print(f" ---Begin Report of Frankenstein--- This book contains {word_count} words, and {organized_characters} ---End Report---")
 if __name__ == "__main__":
     main()
